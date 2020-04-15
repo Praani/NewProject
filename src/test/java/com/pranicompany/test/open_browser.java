0247805960d59f57821cffb6777fec5e5ec29510
Praani <praneshk92@gmail.com>
2020-04-15 12:03:51 +0530

    New Project Changes

---
+++ src/test/java/com/pranicompany/test/open_browser.java
@@ -0,0 +1,57 @@
+package com.pranicompany.test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class open_browser {
+	public WebDriver driver;
+
+	@BeforeTest
+	public void setProperty() {
+		System.setProperty("webdriver.chrome.driver", "/home/pranesh/chromedriver");
+	}
+
+	@Parameters({ "URL" })
+	@BeforeMethod
+	public void OpenURL(String url) {
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("--no-sandbox"); //Bypass OS security model   
+//		options.addArguments("--start-maximized");
+//		options.addArguments("--disable-dev-shm-usage");
+		
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(url);
+	}
+	
+	@Test
+	public void Best_Sellers() throws InterruptedException {
+		WebElement menu = driver.findElement(By.xpath("//div[@id='nav-xshop']"));
+		List<WebElement> links = menu.findElements(By.tagName("a"));
+		int count = links.size();
+		for (int i = 0; i < count; i++) {
+			System.out.println(links.get(i).getText());
+		}
+	}
+	
+	@AfterMethod
+	public void close() {
+		driver.quit();
+	}
+	
+
+	
+}
